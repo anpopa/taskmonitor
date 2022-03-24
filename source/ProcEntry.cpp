eea5065da1a7b9fb01a2ce386f98208e782ec064
Alin Popa <alin.popa@fxdata.ro>
2022-03-24 14:26:43 +0100

    Update license to MIT

--- source/ProcEntry.cpp
+++ source/ProcEntry.cpp
@@ -1,23 +1,12 @@
-/*
- * SPDX license identifier: GPL-2.0-or-later
- *
- * Copyright (C) 2021 Alin Popa
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * \author Alin Popa <alin.popa@fxdata.ro>
+/*-
+ * SPDX-License-Identifier: MIT
+ *-
+ * @date      2021-2022
+ * @author    Alin Popa <alin.popa@fxdata.ro>
+ * @copyright MIT
+ * @brief     ProcEntry Class
+ * @details   Represent per process statistics
+ *-
  */
 
 #include "ProcEntry.h"
