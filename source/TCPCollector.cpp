4fcdb100bed3366b7c3f566b96730113d224f3e6
Alin Popa <alin.popa@fxdata.ro>
2022-07-26 06:37:38 +0200

    Add active cpu cores in session info

--- source/TCPCollector.cpp
+++ source/TCPCollector.cpp
@@ -148,6 +148,7 @@ static bool doCreateSession(const std::shared_ptr<TCPCollector> collector)
             << " jenkinsHash: " << tkm::jnkHsh(idContent.c_str());
   collector->getDescriptor().set_id(std::to_string(tkm::jnkHsh(idContent.c_str())));
   collector->getSessionInfo().set_hash(collector->getDescriptor().id());
+  collector->getSessionInfo().set_core_count(sysconf(_SC_NPROCESSORS_ONLN));
   logInfo() << "Send new sessionID=" << collector->getSessionInfo().hash();
 
   collector->getSessionInfo().set_fast_lane_interval(App()->getFastLaneInterval());
